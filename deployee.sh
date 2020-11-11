a50a628fcec5e4152fd7ac41127e7e1c9b1024a0
Ankit <imankitdevops@gmail.com>
2020-11-11 09:53:42 +0000

    adding deployee and java file

---
+++ deployee.sh
@@ -0,0 +1,2 @@
+This is main file
+this file is contains all rules of deployee
---
+++ java.sh
@@ -0,0 +1,3 @@
+This file is related to java
+all the rules related to java we keep here
+being a java programmer is really tough in it
